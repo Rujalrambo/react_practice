d84b22bf0e50ad8f2835e93cf7f72159b6b3efbe
Rujal <rujalshrsth@gmail.com>
2024-06-11 20:11:20 +0545

    new files

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  let [counter, setCounter] = useState(15)
+
+  // let counter = 15
+
+  const addValue = function(){
+    // counter = counter + 1;
+    if (counter >= 20) {
+      alert('process limit reached')
+    }
+    else{
+    setCounter(counter + 1)
+    }
+  }
+    
+  const removeValue = () => {
+    if (counter <= 0){
+      alert('it has reached its limit')
+    }
+    else{
+    setCounter(counter - 1)
+
+    }
+  }
+
+  return (
+    <>
+    <h1> new file</h1>
+    <h1> new value: {counter}</h1>
+
+    <button onClick={addValue}>add value: {counter}</button>
+    <br/>
+    <button onClick={removeValue}>remove value: {counter}</button>
+    <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
